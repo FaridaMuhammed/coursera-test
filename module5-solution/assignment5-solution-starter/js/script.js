f925de0f9c1355548bf2649dda219f42334796e5
FaridaMuhammed <ftoome545@gmail.com>
2022-03-17 20:39:09 +0300

    I'm just fiexed something_1

--- module5-solution/assignment5-solution-starter/js/script.js
+++ module5-solution/assignment5-solution-starter/js/script.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view. I found it in lecture 60
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl,
+  homeHtmlUrl,
   // function (responseText) {
   //   document.querySelector("#main-content").innerHTML = responseText;
   // },
